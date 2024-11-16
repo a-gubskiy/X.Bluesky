fccee08e07c2a9aad6831f42eecd4428e5d6c76e
Andrew Gubskiy <andrew.gubskiy@outlook.com>
2024-11-16 20:04:05 +0200

    Add comments

--- src/X.Bluesky/MentionResolver.cs
+++ src/X.Bluesky/MentionResolver.cs
@@ -1,3 +1,4 @@
+using JetBrains.Annotations;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Logging.Abstractions;
 using Newtonsoft.Json.Linq;
@@ -6,6 +7,11 @@ namespace X.Bluesky;
 
 public interface IMentionResolver
 {
+    /// <summary>
+    /// Resolve mention to DID format
+    /// </summary>
+    /// <param name="mention"></param>
+    /// <returns></returns>
     Task<string> ResolveMention(string mention);
 }
 
@@ -16,52 +22,72 @@ public class MentionResolver : IMentionResolver
 {
     private readonly IHttpClientFactory _httpClientFactory;
     private readonly ILogger _logger;
-    
+
+    /// <summary>
+    /// Create a new instance of MentionResolver
+    /// </summary>
+    [PublicAPI]
     public MentionResolver()
         : this(new BlueskyHttpClientFactory(), new NullLogger<MentionResolver>())
     {
     }
-    
+
+    /// <summary>
+    /// Create a new instance of MentionResolver
+    /// </summary>
+    /// <param name="httpClientFactory"></param>
+    [PublicAPI]
     public MentionResolver(IHttpClientFactory httpClientFactory)
         : this(httpClientFactory, new NullLogger<MentionResolver>())
     {
     }
 
+    /// <summary>
+    /// Create a new instance of MentionResolver
+    /// </summary>
+    /// <param name="httpClientFactory"></param>
+    /// <param name="logger"></param>
+    [PublicAPI]
     public MentionResolver(IHttpClientFactory httpClientFactory, ILogger logger)
     {
         _httpClientFactory = httpClientFactory;
         _logger = logger;
     }
 
+    /// <summary>
+    /// Create a new instance of MentionResolver
+    /// </summary>
+    /// <param name="httpClientFactory"></param>
+    /// <param name="logger"></param>
+    [PublicAPI]
     public MentionResolver(IHttpClientFactory httpClientFactory, ILogger<MentionResolver> logger)
         : this(httpClientFactory, (ILogger)logger)
     {
     }
 
+    /// <inheritdoc />
     public async Task<string> ResolveMention(string mention)
     {
         var httpClient = _httpClientFactory.CreateClient();
 
         const string url = "https://bsky.social/xrpc/com.atproto.identity.resolveHandle";
-        
+
         var requestUri = $"{url}?handle={mention.Replace("@", string.Empty)}";
-        
+
         var response = await httpClient.GetAsync(requestUri);
 
         var responseContent = await response.Content.ReadAsStringAsync();
 
-        if (response.IsSuccessStatusCode)
-        {
-            var json = JObject.Parse(responseContent);
-
-            return json["did"]?.ToString() ?? string.Empty;
-        }
-        else
+        if (!response.IsSuccessStatusCode)
         {
             _logger.LogError(responseContent);
+
+            // Return null if unable to resolve
+            return string.Empty;
         }
 
-        // Return null if unable to resolve
-        return string.Empty;
+        var json = JObject.Parse(responseContent);
+
+        return json["did"]?.ToString() ?? string.Empty;
     }
 }
